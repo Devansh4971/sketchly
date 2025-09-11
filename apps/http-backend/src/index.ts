7c348d9978fe1d23916666bddd474efa91656ccf
Devansh4971 <devanshwalecha93@gmail.com>
2025-09-11 15:34:38 +0530

    added signup point

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,14 +3,33 @@ import Jwt  from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
 import { createUserSchema , signInSchema,roomSchema} from "@repo/common/types";
+import { prismaClient } from "@repo/db/client";
+import { User } from '../../../packages/db/src/generated/prisma/index';
+import { data } from "motion/react-client";
+
 
 const app=express();
+app.use(express.json())
 const port=3001;
-app.post('/signup',(req,res)=>{
-    const data=createUserSchema.safeParse(req.body)
-    if(!data.success){
+app.post('/signup',async (req,res)=>{
+    const parsedData=createUserSchema.safeParse(req.body)
+    if(!parsedData.success){
         return res.json({
             message:"Wrong credentials"
+        })
+    }
+    try{
+            await prismaClient.User.create({
+             data:{
+                email:parsedData.data?.userName,
+                password:parsedData.data?.password,
+                name:parsedData.data?.name
+            
+                }   
+    })
+    }catch(e){
+        res.status(411).json({
+            message:"User already exist with these credentials"
         })
     }
     res.json({
